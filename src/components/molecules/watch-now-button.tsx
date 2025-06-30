7221404553c1699b9574fe85d1ac4f1b2d931a70
Chrystian Santos <chrystian_santos_silva@hotmail.com>
2025-06-30 20:35:32 -0300

    feat: redirects to max video player

--- src/components/molecules/watch-now-button.tsx
+++ src/components/molecules/watch-now-button.tsx
@@ -3,7 +3,9 @@ import { NavigateButton } from "../atoms";
 
 export function WatchNowButton() {
   return (
-    <NavigateButton href={"/"}>
+    <NavigateButton
+      href={process.env.NEXT_PUBLIC_PLAYER_VIDEO_URL!}
+    >
       Assista agora
       <Play className="size-4" fill="#52525c" />
     </NavigateButton>
